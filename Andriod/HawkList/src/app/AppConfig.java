c2380555eb2bb90ed81b332cf1a98272d6b57077
Vinodh Kannan <vksadayamuthu@hotmail.com>
2015-12-03 02:22:24 -0600

    Updated Andriod Code

---
+++ Andriod/HawkList/src/app/AppConfig.java
@@ -0,0 +1,9 @@
+package app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://localhost/HawkList/v1/login";
+ 
+    // Server user register url
+    public static String URL_REGISTER = "http://localhost/HawkList/v1/register";
+}
